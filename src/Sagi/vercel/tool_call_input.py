417831c3434835829db53b8f251181aab48ae37e
klma <klma@cse.cuhk.edu.hk>
2025-09-19 03:13:23 +0000

    add toolcall for hirag_agent

---
+++ src/Sagi/vercel/tool_call_input.py
@@ -0,0 +1,12 @@
+
+from utils.camel_model import CamelModel
+
+
+class RagSearchToolCallInput(CamelModel):
+    query: str
+
+
+class LoadFileToolCallInput(CamelModel):
+    file_name: str
+    file_url: str
+    media_type: str
